e2efd319ee11c1f5758e0773f465b90d72874e02
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 13:44:51 +0100

    reverting canvas changes

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -2,14 +2,16 @@ import React, { useRef, useEffect } from 'react';
 
 const CanvasComponent = ({ width, height }) => {
   const canvasRef = useRef(null);
+  const canvasWidth = 700; // Width of the canvas frame
+  const canvasHeight = 500; // Height of the canvas frame
+  const windowWidth = width; // Width of the window shape
+  const windowHeight = height; // Height of the window shape
   const borderWidth = 10; // Width of the border
   const divisionWidth = 10; // Width of the dividing lines
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
 
     // Clear canvas
     context.clearRect(0, 0, canvasWidth, canvasHeight);
@@ -21,16 +23,16 @@ const CanvasComponent = ({ width, height }) => {
     // Draw outer frame
     context.strokeStyle = '#000'; // Black lines
     context.lineWidth = borderWidth;
-    context.strokeRect(borderWidth / 2, borderWidth / 2, width - borderWidth, height - borderWidth);
+    context.strokeRect(borderWidth / 2, borderWidth / 2, windowWidth + borderWidth, windowHeight + borderWidth);
 
     // Draw dividing lines
     const numberOfDivisions = 3; // Number of divisions
     context.lineWidth = divisionWidth;
     for (let i = 1; i < numberOfDivisions; i++) {
-      const divisionX = (width / numberOfDivisions) * i;
+      const divisionX = (canvasWidth / numberOfDivisions) * i;
       context.beginPath();
       context.moveTo(divisionX, 0);
-      context.lineTo(divisionX, height);
+      context.lineTo(divisionX, canvasHeight);
       context.stroke();
     }
   }, [width, height]);
@@ -38,8 +40,8 @@ const CanvasComponent = ({ width, height }) => {
   return (
     <canvas
       ref={canvasRef}
-      width={width}
-      height={height}
+      width={canvasWidth}
+      height={canvasHeight}
       style={{ border: '1px solid black' }}
     />
   );
