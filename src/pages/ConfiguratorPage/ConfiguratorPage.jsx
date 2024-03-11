6d89ae32b6f0504f71189306b5ea4a157e6774f7
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 15:13:02 +0100

    updated store path

--- src/pages/ConfiguratorPage/ConfiguratorPage.jsx
+++ src/pages/ConfiguratorPage/ConfiguratorPage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
-import createStore from "./store";
-import App from "./components/App";
-import Store from "./store.js";
+import createStore from "../store"; // Adjusted import path
+import App from "../components/App"; // Adjusted import path
 
 function ConfiguratorPage() {
   window.store = createStore();
