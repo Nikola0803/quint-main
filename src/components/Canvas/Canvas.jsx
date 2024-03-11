a434dfa379f849bf9a2a27547bd2f40dc4c5665a
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 13:24:53 +0100

    t

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 
-const CanvasComponent = ({ windowWidth, windowHeight }) => {
+const CanvasComponent = ({ width, height }) => {
   const canvasRef = useRef(null);
   const canvasWidth = 700; // Width of the canvas frame
   const canvasHeight = 500; // Height of the canvas frame
+  const windowWidth = width; // Width of the window shape
+  const windowHeight = height; // Height of the window shape
   const borderWidth = 10; // Width of the border
   const divisionWidth = 10; // Width of the dividing lines
 
@@ -21,20 +23,19 @@ const CanvasComponent = ({ windowWidth, windowHeight }) => {
     // Draw outer frame
     context.strokeStyle = '#000'; // Black lines
     context.lineWidth = borderWidth;
-    context.strokeRect(borderWidth / 2, borderWidth / 2, windowWidth - borderWidth, windowHeight - borderWidth);
+    context.strokeRect(borderWidth / 2, borderWidth / 2, canvasWidth - borderWidth, canvasHeight - borderWidth);
 
     // Draw dividing lines
     const numberOfDivisions = 3; // Number of divisions
     context.lineWidth = divisionWidth;
-    const divisionSpacing = (windowWidth - 2 * borderWidth) / (numberOfDivisions + 1);
-    for (let i = 1; i <= numberOfDivisions; i++) {
-      const divisionX = borderWidth + divisionSpacing * i;
+    for (let i = 1; i < numberOfDivisions; i++) {
+      const divisionX = (canvasWidth / numberOfDivisions) * i;
       context.beginPath();
-      context.moveTo(divisionX, borderWidth / 2);
-      context.lineTo(divisionX, windowHeight - borderWidth / 2);
+      context.moveTo(divisionX, 0);
+      context.lineTo(divisionX, canvasHeight);
       context.stroke();
     }
-  }, [windowWidth, windowHeight]);
+  }, [width, height]);
 
   return (
     <canvas
