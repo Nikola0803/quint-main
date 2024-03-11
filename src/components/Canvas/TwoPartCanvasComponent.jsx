aa0aa1aefda4192ab3a76b7f0fcbc80a157b3f05
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 17:24:20 +0100

    outer shape for2part window fix

--- src/components/Canvas/TwoPartCanvasComponent.jsx
+++ src/components/Canvas/TwoPartCanvasComponent.jsx
@@ -16,25 +16,25 @@ const TwoPartCanvasComponent = ({ width, height }) => {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
 
     // Draw outer lines of the rectangle shape
-    const x = (canvasWidth - width) / 2 + borderWidth / 2;
-    const y = (canvasHeight - height) / 2 + borderWidth / 2;
-    const rectWidth = width - borderWidth;
-    const rectHeight = height - borderWidth;
+    const outerX = (canvasWidth - width) / 2 - 8; // Adjusted left position
+    const outerY = (canvasHeight - height) / 2 + borderWidth / 2;
+    const outerWidth = width + 16; // Adjusted width
+    const outerHeight = height - borderWidth;
     context.strokeStyle = '#000'; // Black color
     context.lineWidth = borderWidth;
-    context.strokeRect(x, y, rectWidth, rectHeight);
+    context.strokeRect(outerX, outerY, outerWidth, outerHeight);
 
     // Draw two evenly spaced rectangles within the outermost one
-    const rectangleWidth = (rectWidth - spacing) / numRectangles; // Adjusted for spacing
-    const startX = (canvasWidth - rectWidth) / 2 + spacing / 2; // Start X coordinate
-    const startY = (canvasHeight - rectHeight) / 2 + spacing / 2; // Start Y coordinate
+    const rectangleWidth = (width - spacing) / numRectangles; // Adjusted for spacing
+    const startX = (canvasWidth - width) / 2 + spacing / 2; // Start X coordinate
+    const startY = (canvasHeight - height) / 2 + spacing / 2; // Start Y coordinate
     for (let i = 0; i < numRectangles; i++) {
       const rectX = startX + i * (rectangleWidth + spacing); // Adding spacing
-      context.strokeRect(rectX, startY, rectangleWidth, rectHeight - spacing); // Subtracting spacing
+      context.strokeRect(rectX, startY, rectangleWidth, height - spacing); // Adjusted height
 
       // Add number to the bottom right corner of each rectangle
       const numberX = rectX + rectangleWidth - 15;
-      const numberY = startY + rectHeight - 15;
+      const numberY = startY + height - spacing - 15;
       context.fillStyle = '#000'; // Black color
       context.font = 'bold 16px Arial';
       context.fillText(i + 1, numberX, numberY);
