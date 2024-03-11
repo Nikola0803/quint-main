ad06b9334f63ddf8fc885651f5e93c860379364e
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 17:33:04 +0100

    top bottom fixes

--- src/components/Canvas/TwoPartCanvasComponent.jsx
+++ src/components/Canvas/TwoPartCanvasComponent.jsx
@@ -17,9 +17,9 @@ const TwoPartCanvasComponent = ({ width, height }) => {
 
     // Draw outer lines of the rectangle shape
     const outerX = (canvasWidth - width) / 2 - 8; // Adjusted left position
-    const outerY = (canvasHeight - height) / 2 + borderWidth / 2 - 10; // Adjusted top position
+    const outerY = (canvasHeight - height) / 2 + borderWidth / 2 - 5; // Adjusted top position
     const outerWidth = width + 16 + 10; // Adjusted width with extra spacing on the right
-    const outerHeight = height + 20 + 20 - borderWidth; // Adjusted height with extra spacing on top and bottom
+    const outerHeight = height + 20 - borderWidth + 10; // Adjusted height with extra spacing on top and bottom
     context.strokeStyle = '#000'; // Black color
     context.lineWidth = borderWidth;
     context.strokeRect(outerX, outerY, outerWidth, outerHeight);
@@ -27,7 +27,7 @@ const TwoPartCanvasComponent = ({ width, height }) => {
     // Draw two evenly spaced rectangles within the outermost one
     const rectangleWidth = (width - spacing) / numRectangles; // Adjusted for spacing
     const startX = (canvasWidth - width) / 2 + spacing / 2; // Start X coordinate
-    const startY = (canvasHeight - height) / 2 + spacing / 2 - 10; // Adjusted top position with extra spacing
+    const startY = (canvasHeight - height) / 2 + spacing / 2 - 5; // Adjusted top position with extra spacing
     for (let i = 0; i < numRectangles; i++) {
       const rectX = startX + i * (rectangleWidth + spacing); // Adding spacing
       context.strokeRect(rectX, startY, rectangleWidth, height + 20 - spacing); // Adjusted height with extra spacing
