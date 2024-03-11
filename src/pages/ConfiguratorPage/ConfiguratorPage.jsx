932149152b661dd8a5ab66d58e2b851a754a10cd
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-11 15:14:16 +0100

    t

--- src/pages/ConfiguratorPage/ConfiguratorPage.jsx
+++ src/pages/ConfiguratorPage/ConfiguratorPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
-import createStore from "../store"; // Adjusted import path
+import createStore from "./Users/nikolazivkovic/Downloads/window-app/src/store.js"; // Adjusted import path
 import App from "../components/App"; // Adjusted import path
 
 function ConfiguratorPage() {
