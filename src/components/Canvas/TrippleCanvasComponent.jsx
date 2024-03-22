9c0003198ef35fa0facd5afabf64a2ce9a672a15
Nikola0803 <nikolazivkovic0803@gmail.com>
2024-03-22 13:14:18 +0100

    finalfix finally

--- src/components/Canvas/TrippleCanvasComponent.jsx
+++ src/components/Canvas/TrippleCanvasComponent.jsx
@@ -22,7 +22,11 @@ const TrippleCanvasComponent = ({ width, height, fixedDistribution, width1, widt
           const sideWidth = (width - middleWidth) / 2;
           return [sideWidth, middleWidth, sideWidth];
         } else if (fixedDistribution === 'Manual') {
-          return [parseInt(width1), parseInt(width2), parseInt(width3)];
+          // Scale down the manual input values by a factor of 10
+          const scaledWidth1 = parseInt(width1) / 10;
+          const scaledWidth2 = parseInt(width2) / 10;
+          const scaledWidth3 = parseInt(width3) / 10;
+          return [scaledWidth1, scaledWidth2, scaledWidth3];
         } else {
           // Handle unexpected fixedDistribution values
           console.error('Unexpected fixedDistribution value:', fixedDistribution);
